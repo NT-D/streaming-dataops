bbad51c11b6e67964fcc675d0320dbae721e6e18
Masayuki Ota <masota@microsoft.com>
2020-07-10 06:57:49 +0000

    Add comments

--- stream_app.py
+++ stream_app.py
@@ -7,12 +7,14 @@ from pyspark.sql.types import StructType, StructField, DoubleType
 # spark = SparkSession.builder.master('local').getOrCreate()
 spark = SparkSession.builder.appName('StructredStreamingApp').getOrCreate()
 
-# Azure setting information
+# Get Azure setting information from environmental variable
 EVENTHUB_CONNECTION_STRING = os.getenv('EVENTHUB_CONNECTION_STRING')
 EVENTHUB_NAMESPACE = os.getenv('EVENTHUB_NAMESPACE')
 EVENT_HUB_NAME = os.getenv('EVENT_HUB_NAME') # same as kafka topic
 
-# Kafka stream settings
+# Connect IoT Hub from Spark
+# IoT Hub has Event Hub compatible build-in endpoint. Event Hub is compatible with Kafka.
+# Use Kafka connector in this repo because developers sometimes want to use Kafka in the onpremises.
 EH_SASL = f'org.apache.kafka.common.security.plain.PlainLoginModule required \
     username="$ConnectionString" password="{EVENTHUB_CONNECTION_STRING}";'
 BOOTSTRAP_SERVERS = f'{EVENTHUB_NAMESPACE}:9093'
@@ -28,6 +30,9 @@ rowStreamDf = spark.readStream \
     .select(col("value").cast("STRING"))
 
 # Set and apply schema
+# Created simulator device in another repo and send random temperature and humidity to IoT Hub
+# https://github.com/NT-D/streaming-dataops-device/blob/master/simulator.py
+# This part uses same schema with the repository.
 schema = StructType([
     StructField("temperature", DoubleType(), True),
     StructField("humidity", DoubleType(), True)
