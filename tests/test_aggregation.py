e0d993fc2c14c899e4865a050115bb0dc5d986f6
Masayuki Ota <masota@microsoft.com>
2020-08-06 02:42:43 +0000

    add comment

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -21,6 +21,7 @@ def test_aggregate_sensordata(spark):
     # Write data in the console
     initialDF = (spark
                  .readStream
+                 # Read 1 file/trigger. It emmulcate streaming
                  .option("maxFilesPerTrigger", 1)
                  .schema(schema.eventhub_schema)
                  .json(file_location)
